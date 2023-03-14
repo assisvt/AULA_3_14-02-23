5466de0b524998977aee62bae2963270eb1190aa
Vítor Assis <contatoassisvt@gmail.com>
2023-03-14 08:57:07 -0300

    Update if-statement.py

--- if-statement.py
+++ if-statement.py
@@ -1,1 +1,18 @@
-
+valor = int(input("Digite um número: "))
+if valor >= 100:
+    print("O valor é maior ou igual a cem.")
+else:
+    print("Valor menor do que cem.")
+print("Ademais, o valor que você escolheu foi:", valor)
+---
+# Faça um programa que calcule a média aritmética de um aluno que realizou duas avaliações.
+# Além do valor da média, inclua na tela de saída uma das menssagens: "Aluno aprovado." ou "Aluno reprovado."
+# aprovado: maior ou igual a 5
+# reprovado: menor do que 5
+nota1 = float(input("Qual foi a sua nota na primeira prova? "))
+nota2 = float(input("Qual foi a sua nota na segunda prova? "))
+media = (nota1 + nota2)/2 #parênteses obrigatórios
+if media >= 5:
+    print("Aluno aprovado com média", media)
+else:
+    print("Aluno reprovado com média", media)
